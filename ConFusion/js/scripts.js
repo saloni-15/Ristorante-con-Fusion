2d8551b4c41c36a738832c3665372499f8ee0de3
Saloni <subedisaloni2002@gmail.com>
2021-06-28 19:00:42 +0530

    ⚡ combining two buttons to one

--- ConFusion/js/scripts.js
+++ ConFusion/js/scripts.js
@@ -1,9 +1,17 @@
 $(document).ready(function () {
-  $("#mycarousel").carousel({ interval: 2000 });
-  $("#carousel-pause").click(function () {
-    $("#mycarousel").carousel('pause');
-  });
-  $("#carousel-play").click(function () {
-    $("#mycarousel").carousel('cycle');
+  $("#mycarousel").carousel({ interval: 2500 });
+
+  $("#carouselButton").click(function () {
+    if ($("#carouselButton").children("span").hasClass("fa-pause")) {
+      $("#mycarousel").carousel("pause");
+      $("#carouselButton").children("span").removeClass("fa-pause");
+      $("#carouselButton").children("span").addClass("fa-play");
+    }
+    else if ($("#carouselButton").children("span").hasClass("fa-play")){
+      $("#mycarousel").carousel("cycle");
+      $("#carouselButton").children("span").removeClass("fa-play");
+      $("#carouselButton").children("span").addClass("fa-pause");
+    }
+
   });
 });
